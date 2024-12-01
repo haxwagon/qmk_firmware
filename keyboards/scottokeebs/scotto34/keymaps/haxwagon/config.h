289ee1a5fb6d9228301812679b1ae2574eea9aa3
haxwagon <hawkins22@gmail.com>
2024-12-01 15:30:27 -0600

    Adding some tap dancing and tri layer

--- keyboards/scottokeebs/scotto34/keymaps/haxwagon/config.h
+++ keyboards/scottokeebs/scotto34/keymaps/haxwagon/config.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Define options
 #define ENCODER_A_PINS { GP6 }
 #define ENCODER_B_PINS { GP7 }
-#define JOYSTICK_AXIS_COUNT 5
+#define JOYSTICK_AXIS_COUNT 6
 #define JOYSTICK_AXIS_RESOLUTION 10
 #define JOYSTICK_BUTTON_COUNT 12
 #define JOYSTICK_HAS_HAT
@@ -28,5 +28,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define OLED_DISPLAY_128X64
 #define OLED_DISPLAY_ADDRESS 0x3C
 #define PERMISSIVE_HOLD
-#define TAPPING_TERM 135
+#define TAPPING_TERM 100
 #define TAPPING_TERM_PER_KEY
+#define TAPPING_TOGGLE 2
+#define TRI_LAYER_LOWER_LAYER 1
+#define TRI_LAYER_UPPER_LAYER 2
+#define TRI_LAYER_ADJUST_LAYER 3
--- keyboards/scottokeebs/scotto34/keymaps/haxwagon/keymap.c
+++ keyboards/scottokeebs/scotto34/keymaps/haxwagon/keymap.c
@@ -19,29 +19,52 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // layers
 enum LAYERS {
-    LAYER_QWERTY,
-    LAYER_SPECIAL,
-    LAYER_NUMSYMS,
-    LAYER_EDIT,
+    LAYER_QWERTY,  // default
+    LAYER_FUNC,    // lower
+    LAYER_NUMSYMS, // upper
+    LAYER_MOVE,    // adjust
+    // ** MODES **
     LAYER_MEDIA,
-    LAYER_MOUSE,
-    LAYER_JOYSTICK
+    LAYER_JOYSTICK,
+    LAYER_NUMPAD,
+};
+
+// tap dances
+enum {
+    TD_BRACES,
+    TD_BRACKETS,
+    TD_COMMSCLN,
+    TD_DOTEXLM,
+    TD_PARENS,
+    TD_PASTSLS,
+    TD_PPLSMNS,
+    TD_QUOTGRV,
+    TD_SLASHES,
+};
+tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for Escape, twice for Caps Lock
+    [TD_BRACES]   = ACTION_TAP_DANCE_DOUBLE(KC_LCBR, KC_RCBR),
+    [TD_BRACKETS] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_RBRC),
+    [TD_COMMSCLN] = ACTION_TAP_DANCE_DOUBLE(KC_COMM, KC_SCLN),
+    [TD_DOTEXLM]  = ACTION_TAP_DANCE_DOUBLE(KC_DOT, KC_EXLM),
+    [TD_PARENS]   = ACTION_TAP_DANCE_DOUBLE(KC_LPRN, KC_RPRN),
+    [TD_PASTSLS]  = ACTION_TAP_DANCE_DOUBLE(KC_PAST, KC_PSLS),
+    [TD_PPLSMNS]  = ACTION_TAP_DANCE_DOUBLE(KC_PPLS, KC_PMNS),
+    [TD_QUOTGRV]  = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, KC_GRV),
+    // [TD_SLASHES]  = TODO: fwd and backwards slash with all mods on one key
 };
 
 // key combos
-const uint16_t PROGMEM left_alt_combo[] = {KC_S, KC_D, COMBO_END};
-const uint16_t PROGMEM left_ctl_combo[] = {KC_A, KC_S, COMBO_END};
-const uint16_t PROGMEM left_ctl_alt_combo[] = {KC_A, KC_S, KC_D, COMBO_END};
-const uint16_t PROGMEM left_ctl_gui_combo[] = {KC_A, KC_S, KC_D, KC_F, COMBO_END};
-const uint16_t PROGMEM left_gui_combo[] = {KC_D, KC_F, COMBO_END};
-const uint16_t PROGMEM right_alt_combo[] = {KC_K, KC_L, COMBO_END};
-const uint16_t PROGMEM right_ctl_combo[] = {KC_L, KC_SCLN, COMBO_END};
-const uint16_t PROGMEM right_gui_combo[] = {KC_J, KC_K, COMBO_END};
-const uint16_t PROGMEM right_ctl_alt_combo[] = {KC_K, KC_L, KC_SCLN, COMBO_END};
-const uint16_t PROGMEM right_ctl_gui_combo[] = {KC_J, KC_K, KC_L, KC_SCLN, COMBO_END};
-const uint16_t PROGMEM edit_combo[] = {KC_X, KC_C, COMBO_END};
-const uint16_t PROGMEM mouse_combo[] = {KC_C, KC_V, COMBO_END};
-const uint16_t PROGMEM media_combo[] = {KC_X, KC_C, KC_V, COMBO_END};
+const uint16_t PROGMEM left_alt_combo[]      = {KC_X, KC_C, COMBO_END};
+const uint16_t PROGMEM left_ctl_combo[]      = {KC_C, KC_V, COMBO_END};
+const uint16_t PROGMEM left_ctl_alt_combo[]  = {KC_X, KC_C, KC_V, COMBO_END};
+const uint16_t PROGMEM left_ctl_gui_combo[]  = {KC_C, KC_V, KC_B, COMBO_END};
+const uint16_t PROGMEM left_gui_combo[]      = {KC_V, KC_B, COMBO_END};
+const uint16_t PROGMEM right_alt_combo[]     = {KC_COMM, KC_DOT, COMBO_END};
+const uint16_t PROGMEM right_ctl_combo[]     = {KC_M, KC_COMM, COMBO_END};
+const uint16_t PROGMEM right_gui_combo[]     = {KC_N, KC_M, COMBO_END};
+const uint16_t PROGMEM right_ctl_alt_combo[] = {KC_M, KC_COMM, KC_DOT, COMBO_END};
+const uint16_t PROGMEM right_ctl_gui_combo[] = {KC_N, KC_M, KC_COMM, COMBO_END};
 combo_t key_combos[] = {
     COMBO(left_alt_combo, KC_LALT),
     COMBO(left_ctl_combo, KC_LCTL),
@@ -53,23 +76,22 @@ combo_t key_combos[] = {
     COMBO(right_gui_combo, KC_RGUI),
     COMBO(right_ctl_alt_combo, RCTL(KC_RALT)),
     COMBO(right_ctl_gui_combo, RCTL(KC_RGUI)),
-    COMBO(edit_combo, MO(3)),
-    COMBO(media_combo, MO(4)),
-    COMBO(mouse_combo, MO(5)),
 };
 
 // key overrides
-const key_override_t f11_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_F1, KC_F11);
-const key_override_t f12_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_F2, KC_F12);
-const key_override_t f13_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_F3, KC_F13);
-const key_override_t f14_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_F4, KC_F14);
-const key_override_t f15_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_F5, KC_F15);
-const key_override_t f16_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_F6, KC_F16);
-const key_override_t f17_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_F7, KC_F17);
-const key_override_t f18_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_F8, KC_F18);
-const key_override_t f19_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_F9, KC_F19);
-const key_override_t f20_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_F10, KC_F20);
-const key_override_t *key_overrides[] = {
+const key_override_t backspace_del_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
+const key_override_t f11_key_override           = ko_make_basic(MOD_MASK_SHIFT, KC_F1, KC_F11);
+const key_override_t f12_key_override           = ko_make_basic(MOD_MASK_SHIFT, KC_F2, KC_F12);
+const key_override_t f13_key_override           = ko_make_basic(MOD_MASK_SHIFT, KC_F3, KC_F13);
+const key_override_t f14_key_override           = ko_make_basic(MOD_MASK_SHIFT, KC_F4, KC_F14);
+const key_override_t f15_key_override           = ko_make_basic(MOD_MASK_SHIFT, KC_F5, KC_F15);
+const key_override_t f16_key_override           = ko_make_basic(MOD_MASK_SHIFT, KC_F6, KC_F16);
+const key_override_t f17_key_override           = ko_make_basic(MOD_MASK_SHIFT, KC_F7, KC_F17);
+const key_override_t f18_key_override           = ko_make_basic(MOD_MASK_SHIFT, KC_F8, KC_F18);
+const key_override_t f19_key_override           = ko_make_basic(MOD_MASK_SHIFT, KC_F9, KC_F19);
+const key_override_t f20_key_override           = ko_make_basic(MOD_MASK_SHIFT, KC_F10, KC_F20);
+const key_override_t *key_overrides[]           = {
+    &backspace_del_key_override,
 	&f11_key_override,
 	&f12_key_override,
 	&f13_key_override,
@@ -79,7 +101,7 @@ const key_override_t *key_overrides[] = {
 	&f17_key_override,
 	&f18_key_override,
 	&f19_key_override,
-	&f20_key_override
+	&f20_key_override,
 };
 
 // joystick settings
@@ -89,24 +111,33 @@ joystick_config_t joystick_axes[JOYSTICK_AXIS_COUNT] = {
     JOYSTICK_AXIS_VIRTUAL,
     JOYSTICK_AXIS_VIRTUAL,
     JOYSTICK_AXIS_VIRTUAL,
-    JOYSTICK_AXIS_VIRTUAL
+    JOYSTICK_AXIS_VIRTUAL,
+    JOYSTICK_AXIS_VIRTUAL,
 };
 static bool joystick_precision = false;
 static uint16_t joystick_precision_mod = 64;
 static uint16_t joystick_axis_val = 127;
 
+enum JS_AXES {
+    JS_AXIS_0_X,
+    JS_AXIS_0_Y,
+    JS_AXIS_0_Z,
+    JS_AXIS_1_X,
+    JS_AXIS_1_Y,
+    JS_AXIS_1_Z
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_QWERTY] = LAYOUT_split_3x5_2(
-        KC_Q,         KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,     KC_O,   KC_P,
-        KC_A,         KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,     KC_L,   KC_SCLN,
-        LSFT_T(KC_Z), KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, RSFT_T(KC_SLSH),
-        LT(LAYER_NUMSYMS, KC_SPACE), LT(LAYER_SPECIAL, KC_SPACE), LT(LAYER_SPECIAL, KC_SPACE), LT(LAYER_NUMSYMS, KC_SPACE)
+        KC_Q,         KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,            KC_O,           KC_P,
+        KC_A,         KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,            KC_L,           KC_SCLN,
+        LSFT_T(KC_Z), KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, TD(TD_COMMSCLN), TD(TD_DOTEXLM), RSFT_T(KC_SLSH),
+        LT(TL_UPPR, KC_SPACE), LT(TL_LOWR, KC_SPACE), LT(TL_LOWR, KC_SPACE), LT(TL_UPPR, KC_SPACE)
     ),
-    [LAYER_SPECIAL] = LAYOUT_split_3x5_2(
-        KC_ESC,         KC_NO,   KC_NO,   KC_NO,   TG(LAYER_JOYSTICK), KC_MINS, KC_PLUS, KC_EQL,  KC_DEL,  KC_BSPC,
-        KC_TAB,         KC_NO,   KC_NO,   KC_NO,   KC_NO,              KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT,
-        LSFT_T(KC_GRV), KC_NO,   KC_LCBR, KC_RCBR, KC_NO,              KC_NO,   KC_LBRC, KC_RBRC, KC_QUOT, RSFT_T(KC_BSLS),
+    [LAYER_FUNC] = LAYOUT_split_3x5_2(
+        KC_ESC,         KC_NO,   TT(LAYER_MEDIA),  TT(LAYER_NUMPAD), TT(LAYER_JOYSTICK), KC_NO,         KC_MINS,         KC_PLUS,       KC_EQL,         KC_BSPC,
+        KC_TAB,         KC_MENU, KC_HOME,          KC_FIND,          KC_NO,              KC_LEFT,       KC_DOWN,         KC_UP,         KC_RGHT,        KC_ENT,
+        LSFT_T(KC_GRV), KC_NO,   KC_NO,            KC_NO,            KC_NO,              TD(TD_BRACES), TD(TD_BRACKETS), TD(TD_PARENS), TD(TD_QUOTGRV), RSFT_T(KC_BSLS),
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
     [LAYER_NUMSYMS] = LAYOUT_split_3x5_2(
@@ -115,22 +146,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LSFT_T(KC_EXLM), KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, RSFT_T(KC_RPRN),
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
-    [LAYER_EDIT] = LAYOUT_split_3x5_2(
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LCTL(KC_X), LCTL(KC_Z), KC_UP,   KC_DEL,  KC_INS,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LCTL(KC_C), KC_LEFT,    KC_DOWN, KC_RGHT, KC_ENT,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LCTL(KC_V), KC_PGDN,    KC_PGUP, KC_HOME, KC_END,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-    ),
-    [LAYER_MEDIA] = LAYOUT_split_3x5_2(
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_MRWD, KC_VOLU, KC_MFFD, KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MPLY, KC_MPRV, KC_VOLD, KC_MNXT, KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_MUTE, KC_NO,   KC_NO,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-    ),
-    [LAYER_MOUSE] = LAYOUT_split_3x5_2(
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, MS_BTN5, MS_BTN2, MS_BTN3, MS_BTN4, MS_BTN6,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, MS_LEFT, MS_DOWN, MS_UP,   MS_RGHT, MS_BTN1,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, MS_BTN7,
+    [LAYER_MOVE] = LAYOUT_split_3x5_2(
+        MS_WHLU, MS_WHLL, MS_UP,   MS_WHLR, MS_BTN2, KC_NO,   KC_NO,    KC_NO,   KC_DEL,  KC_INS,
+        MS_WHLD, MS_LEFT, MS_DOWN, MS_RGHT, MS_BTN1, KC_LEFT, KC_DOWN,  KC_UP,   KC_RGHT, KC_ENT,
+        MS_BTN7, MS_BTN6, MS_BTN5, MS_BTN4, MS_BTN3, KC_HOME, KC_PGDN,  KC_PGUP, KC_END,  KC_NO,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
     [LAYER_JOYSTICK] = LAYOUT_split_3x5_2(
@@ -138,7 +157,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         JS_6,  KC_P4, KC_P5, KC_P6, JS_7, JS_1, JS_0, JOYSTICK_HAT_WEST,      JOYSTICK_HAT_CENTER, JOYSTICK_HAT_EAST,
         JS_11, KC_P1, KC_P2, KC_P3, JS_9, JS_5, JS_4, JOYSTICK_HAT_SOUTHWEST, JOYSTICK_HAT_SOUTH,  JOYSTICK_HAT_SOUTHEAST,
         TG(LAYER_JOYSTICK), TG(LAYER_JOYSTICK), TG(LAYER_JOYSTICK), TG(LAYER_JOYSTICK)
-    )
+    ),
+    [LAYER_MEDIA] = LAYOUT_split_3x5_2(
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MRWD, KC_MUTE, KC_NO,   KC_MFFD, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+    [LAYER_NUMPAD] = LAYOUT_split_3x5_2(
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TD(TD_PASTSLS), KC_P7, KC_P8, KC_P9, KC_BSPC,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TD(TD_PPLSMNS), KC_P4, KC_P5, KC_P6, KC_PENT,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P0,          KC_P1, KC_P2, KC_P3, KC_PDOT,
+        TG(LAYER_NUMPAD), TG(LAYER_NUMPAD), TG(LAYER_NUMPAD), TG(LAYER_NUMPAD)
+    ),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -147,36 +178,30 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         precision_val -= joystick_precision_mod;
     }
 
-    switch (keycode) {
-        case KC_P4:
-            joystick_set_axis(0, record->event.pressed ? -precision_val : 0);
-            return false;
-        case KC_P6:
-            joystick_set_axis(0, record->event.pressed ? precision_val : 0);
-            return false;
-        case KC_P8:
-            joystick_set_axis(1, record->event.pressed ? -precision_val : 0);
-            return false;
-        case KC_P2:
-        case KC_P5:
-            joystick_set_axis(1, record->event.pressed ? precision_val : 0);
-            return false;
-        case KC_P7:
-            joystick_set_axis(2, record->event.pressed ? -precision_val : 0);
-            return false;
-        case KC_P1:
-            joystick_set_axis(2, record->event.pressed ? precision_val : 0);
-            return false;
-        case KC_P9:
-            joystick_set_axis(3, record->event.pressed ? -precision_val : 0);
-            return false;
-        case KC_P3:
-            joystick_set_axis(3, record->event.pressed ? precision_val : 0);
-            return false;
-        case KC_P0:
-            joystick_precision = record->event.pressed;
-            return false;
-    }
+    // switch (keycode) {
+    //     case KC_P4:
+    //         joystick_set_axis(JS_AXIS_0_X, record->event.pressed ? -precision_val : 0);
+    //         return false;
+    //     case KC_P6:
+    //         joystick_set_axis(JS_AXIS_0_X, record->event.pressed ? precision_val : 0);
+    //         return false;
+    //     case KC_P8:
+    //         joystick_set_axis(JS_AXIS_0_Y, record->event.pressed ? -precision_val : 0);
+    //         return false;
+    //     case KC_P2:
+    //     case KC_P5:
+    //         joystick_set_axis(JS_AXIS_0_Y, record->event.pressed ? precision_val : 0);
+    //         return false;
+    //     case KC_P7:
+    //         joystick_set_axis(JS_AXIS_Z, record->event.pressed ? -precision_val : 0);
+    //         return false;
+    //     case KC_P9:
+    //         joystick_set_axis(JS_AXIS_Z, record->event.pressed ? precision_val : 0);
+    //         return false;
+    //     case KC_P0:
+    //         joystick_precision = record->event.pressed;
+    //         return false;
+    // }
     return true;
 }
 
@@ -235,13 +260,13 @@ bool oled_task_user(void) {
             oled_write_P(PSTR("Default\n"), false);
             oled_write_P(PSTR(" Q  W  E  R  T  Y  U  I  O  P\n"), false);
             oled_write_P(PSTR(" A  S  D  F  G  H  J  K  L  ;\n"), false);
-            oled_write_P(PSTR(" Z  X  C  V  B  N  M  ,  .  /\n"), false);
+            oled_write_P(PSTR(" Z  X  C  V  B  N  M ,; .!  /\n"), false);
             break;
-        case LAYER_SPECIAL:
-            oled_write_P(PSTR("Special\n"), false);
-            oled_write_P(PSTR("ES          JS  -  +  = DE <-\n"), false);
+        case LAYER_FUNC:
+            oled_write_P(PSTR("Functions\n"), false);
+            oled_write_P(PSTR("ES    ME NP JS     -  +  = <-\n"), false);
             oled_write_P(PSTR("->              <  v  ^  > EN\n"), false);
-            oled_write_P(PSTR(" `     {  }        [  ]  '  \\\n"), false);
+            oled_write_P(PSTR(" `             {} [] () '`  \\\n"), false);
             break;
         case LAYER_NUMSYMS:
             oled_write_P(PSTR("Numbers & Symbols\n"), false);
@@ -249,20 +274,29 @@ bool oled_task_user(void) {
             oled_write_P(PSTR(" 1  2  3  4  5  6  7  8  9  0\n"), false);
             oled_write_P(PSTR(" !  @  #  $  %  ^  &  *  (  )\n"), false);
             break;
-        case LAYER_EDIT:
-            oled_write_P(PSTR("Edit\n"), false);
-            break;
-        case LAYER_MEDIA:
-            oled_write_P(PSTR("Media\n"), false);
-            break;
-        case LAYER_MOUSE:
-            oled_write_P(PSTR("Mouse\n"), false);
+        case LAYER_MOVE:
+            oled_write_P(PSTR("Move\n"), false);
+            oled_write_P(PSTR("WU WL MU WR M2          DE IN\n"), false);
+            oled_write_P(PSTR("WD ML MD MR M1  <  v  ^  > EN\n"), false);
+            oled_write_P(PSTR("M7 M6 M5 M4 M3 HM PD PU ED   \n"), false);
             break;
         case LAYER_JOYSTICK:
             oled_write_P(PSTR("Joystick\n"), false);
-            oled_write_P(PSTR("10     ^     8  3  2 NW  N NE\n"), false);
-            oled_write_P(PSTR(" 6  <  v  >  7  1  0  W  C  E\n"), false);
-            oled_write_P(PSTR("11     v     9  5  4 SW  S SE\n"), false);
+            oled_write_P(PSTR("10 z< /\\ z>  8  3  2 NW  N NE\n"), false);
+            oled_write_P(PSTR(" 6 <= \\/ =>  7  1  0  W  C  E\n"), false);
+            oled_write_P(PSTR("11    \\/     9  5  4 SW  S SE\n"), false);
+            break;
+        case LAYER_MEDIA:
+            oled_write_P(PSTR("Media\n"), false);
+            oled_write_P(PSTR("                             \n"), false);
+            oled_write_P(PSTR("               |< VD VU >| EN\n"), false);
+            oled_write_P(PSTR("               << MU    >>   \n"), false);
+            break;
+        case LAYER_NUMPAD:
+            oled_write_P(PSTR("Numpad\n"), false);
+            oled_write_P(PSTR("               */  7  8  9 <-\n"), false);
+            oled_write_P(PSTR("               +-  4  5  6 EN\n"), false);
+            oled_write_P(PSTR("                0  1  2  3  .\n"), false);
             break;
         default:
             // Or use the write_ln shortcut over adding '\n' to the end of your string
